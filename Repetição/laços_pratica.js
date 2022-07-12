9c78759a2fb8182c27d86c8f94eba681988f2e48
Gustavo Rafael <96211815+Kyoudan@users.noreply.github.com>
2022-07-12 11:10:20 -0300

    Update and rename laços_pratica.html to laços_pratica.js

---
+++ Repetição/laços_pratica.js
@@ -0,0 +1,8 @@
+
+        var frutas = ['Banana', 'Maça', 'Manga', 'Uva', 'Pera', 'Abacaxi', 'Abacate', 'Etc...']
+        console.log(frutas)
+
+    while(length < frutas.length){
+        document.write(frutas + '<br />')
+        frutas++
+    }
