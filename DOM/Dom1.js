8c60fa9a6e2104c8b6414a15d6fce7eb01bd4a6e
Gustavo Rafael <96211815+Kyoudan@users.noreply.github.com>
2022-07-12 11:07:07 -0300

    Update and rename Dom1.html to Dom1.js

---
+++ DOM/Dom1.js
@@ -0,0 +1,5 @@
+
+        //console.log(document.getElementById('teste2'))
+        //console.log(document.getElementsByClassName('teste1'))
+        //console.log(document.getElementsByTagName('teste2'))
+        console.log(document.getElementsByName('teste3'))
