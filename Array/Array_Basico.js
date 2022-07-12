cb28661570dd5e1775c30853eee789c3c6a99ec9
Gustavo Rafael <96211815+Kyoudan@users.noreply.github.com>
2022-07-12 11:02:34 -0300

    Update and rename Array_Basico.html to Array_Basico.js

---
+++ Array/Array_Basico.js
@@ -0,0 +1,13 @@
+
+        //var lista = Array()
+        var lista = []
+
+        lista[1] = 'Pao'
+        lista[2] = 'Gustavo'
+        lista[3] = 'Yasmim'
+
+        console.log(lista[2])
+
+        var lista1 = ['banana', 'maça', 'outros']
+
+        console.log(lista1[0])
