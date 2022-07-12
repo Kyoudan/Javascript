cdedebd792a1fa39b7ebad15a85a0b5c951cc205
Gustavo Rafael <96211815+Kyoudan@users.noreply.github.com>
2022-07-12 11:09:53 -0300

    Update and rename for.html to for.js

---
+++ Repetição/for.js
@@ -0,0 +1,8 @@
+
+        // for (variavel, condição, parada){}
+        for(var x = 10; x >= 0; x--){
+            document.write(x + '<br />')
+
+            //break
+            //continue
+        }
