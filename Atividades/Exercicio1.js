eecac49f5b5d473c36bb1b3e091a605ea0ef2ab9
Gustavo Rafael <96211815+Kyoudan@users.noreply.github.com>
2022-07-12 11:04:52 -0300

    Update and rename Exercicio1.html to Exercicio1.js

--- Atividades/Exercicio1.js
+++ Atividades/Exercicio1.js
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script>
+
         var a = 10
         var b = 20
         var c = null
@@ -51,8 +44,3 @@
         document.write('C: ' + f + '<br />')
 
         
-    </script>
-</head>
-<body>
-</body>
-</html>
