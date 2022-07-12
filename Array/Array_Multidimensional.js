1360f7ef8602c19a952033db4108580354813089
Gustavo Rafael <96211815+Kyoudan@users.noreply.github.com>
2022-07-12 11:03:23 -0300

    Update and rename Array_Multidimensional.html to Array_Multidimensional.js

---
+++ Array/Array_Multidimensional.js
@@ -0,0 +1,8 @@
+
+        var lista = Array()
+        lista['frutas'] = ['Morango', 'Uva', 'Abacaxi']
+        lista['pessoas'] = ['Gustavo', 'Carlos', 'Yasmim']
+
+        console.log(lista)
+
+        document.write(lista['frutas'][0])
