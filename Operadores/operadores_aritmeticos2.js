21fac2d203928d2c5a0f072fd3b8cc8b1e324dee
Gustavo Rafael <96211815+Kyoudan@users.noreply.github.com>
2022-07-12 11:12:03 -0300

    Update and rename operadores_aritmeticos2.html to operadores_aritmeticos2.js

--- Operadores/operadores_aritmeticos2.js
+++ Operadores/operadores_aritmeticos2.js
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script>
+
         var teste = 15
 
         //teste = teste + 5
@@ -21,9 +14,3 @@
         letras += ', Tudo bom?'
 
         document.write(letras)
-    </script>
-</head>
-<body>
-    
-</body>
-</html>
