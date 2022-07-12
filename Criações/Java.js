81a42db6d7de7366454ef4b13e04ca47252dc4bd
Gustavo Rafael <96211815+Kyoudan@users.noreply.github.com>
2022-07-12 11:06:00 -0300

    Update and rename Java.html to Java.js

--- Criações/Java.js
+++ Criações/Java.js
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script>
+
         function num1(primeironumero){
             var numero = document.getElementById('num1').value
             var numero = parseInt(numero)
@@ -48,14 +41,3 @@
         }
         
 
-    </script>
-</head>
-<body>
-    <input type="text" id="num1" placeholder="Numero" onblur="num1(primeironumero)">
-    <input type="text" id="num2" placeholder="Numero" onblur="num2(segundonumero)">
-    <br>
-    <input type="text" id="operacao" placeholder="Coloque a operação aqui" onblur="foco()">
-    </select>
-    <button onclick="clique(numero1, numero2, resultado)" style="margin-left: 200px;">&#9745;</button>
-</body>
-</html>
