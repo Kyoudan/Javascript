1651592a4faf798c76a3b25f16507892ef2f3f58
Gustavo Rafael <96211815+Kyoudan@users.noreply.github.com>
2022-07-12 11:03:40 -0300

    Update and rename Array_Pesquisa.html to Array_Pesquisa.js

--- Array/Array_Pesquisa.js
+++ Array/Array_Pesquisa.js
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script>
+
         var lista = ['Banana', 'Maça', 'Uva', 'Pera']
         var auxiliar = lista.indexOf('Uva')
 
@@ -14,9 +7,3 @@
         } else {
             document.write('Esse elemento existe e está na posição: ' + auxiliar)
         }
-    </script>
-</head>
-<body>
-    
-</body>
-</html>
