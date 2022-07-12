86f0bf083e62d3f07d9bea3635ee4e3cb862f488
Gustavo Rafael <96211815+Kyoudan@users.noreply.github.com>
2022-07-12 11:05:11 -0300

    Update and rename atividade03.html to atividade03.js

--- Atividades/atividade03.js
+++ Atividades/atividade03.js
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-    <script>
+
         var nome = prompt('Digite seu nome')
         var altura = prompt('Digite sua altura em cm')
         var peso = prompt('Digite seu peso')
@@ -41,8 +33,4 @@
 
         document.write(nome + ' possui índice de massa corporal igual a ' + massa + ', sendo classificado como: ' + resultado)
 
-    </script>
-<body>
-    
-</body>
-</html>
+
